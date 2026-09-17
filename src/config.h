a7bb0a003d87bbf92992c75f47f3ec9ddddad9df
LouSkull <146044959+LouSkull@users.noreply.github.com>
2026-09-17 14:58:26 -0500

    Delete src directory

--- src/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "core.h"
-#include <string>
-Settings readSettings(const std::wstring& path,const Settings& defaults=Settings{},bool includeSecurity=true);
-bool writeSettings(const std::wstring& path,const Settings& settings,bool includeSecurity=true);
-void copyAppearance(Settings& to,const Settings& from);
